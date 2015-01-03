c03ed0e8bb51ad36dcb98a5cd323fc39bc328cc9
Jonathan Powers <jpowers@gmail.com>
2015-01-03 00:00:49 -0500

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,8 @@
-require 'codeclimate-test-reporter'
 require 'simplecov'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  CodeClimate::TestReporter::Formatter,
   Coveralls::SimpleCov::Formatter,
   SimpleCov::Formatter::HTMLFormatter
 ]
-SimpleCov.start 'rails'
+SimpleCov.start
